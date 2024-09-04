1408052d2c95da70d72292c577b16c3a1a507a62
muyenzi10 <muyenzija10@gmail.com>
2024-09-04 06:56:33 -0700

    updated

---
+++ utils/redis.js
@@ -0,0 +1,39 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+/**
+ * Class for performing operatio
+ */
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.getAsync = promisify(this.client.get).bind(this.client);
+
+    this.client.on('error', (error) => {
+      console.log(`Redis client not connected to the server: ${error.message}`);
+    });
+
+    this.client.on('connect', () => {
+    });
+  }
+
+
+  isAlive() {
+    return this.client.connected;
+}
+  async get(key) {
+    const value = await this.getAsync(key);
+    return value;
+}
+  async set(key, value, duration) {
+    this.client.setex(key, duration, value);
+  }
+
+  async del(key) {
+    this.client.del(key);
+  }
+}
+
+const redisClient = new RedisClient();
+
+export default redisClient;
